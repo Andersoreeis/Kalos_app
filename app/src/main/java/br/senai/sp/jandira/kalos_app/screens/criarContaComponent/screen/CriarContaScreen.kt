3bf98f4475f51dc9b43f0b3f170bd93db952fb6f
CLAUDIO HENRIQUE FERREIRA DE SOUSA <22282470@dsn127m1246728.cfp127aluno.local>
2023-09-06 15:47:21 -0300

    terminando tela de cadastro

--- app/src/main/java/br/senai/sp/jandira/kalos_app/screens/criarContaComponent/screen/CriarContaScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/kalos_app/screens/criarContaComponent/screen/CriarContaScreen.kt
@@ -1,6 +1,7 @@
 package br.senai.sp.jandira.kalos_app.screens.criarContaComponent.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +14,8 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,6 +24,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import br.senai.sp.jandira.app_kalos.components.createButton
 import br.senai.sp.jandira.app_kalos.components.createButtonWithWidth
@@ -31,52 +35,68 @@ import br.senai.sp.jandira.kalos_app.screens.criarContaComponent.components.Head
 import br.senai.sp.jandira.kalos_app.ui.theme.GreenKalos
 
 @Composable
-fun CriarContaScreen( navController: NavController) {
-    Column (
+fun CriarContaScreen(navController: NavController) {
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
             .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 25.dp, start = 21.dp)
         ) {
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 25.dp, start = 21.dp)
+            IconButton(
+                onClick = { navController.navigate("fazerLogin") }
             ) {
-                IconButton(
-                    onClick = { /*TODO*/ }
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_chevron_left_24) ,
-                        contentDescription = "Botão para voltar para tela anterior",
-                        tint = Color.White,
-                    )
-                }
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_chevron_left_24),
+                    contentDescription = "Botão para voltar para tela anterior",
+                    tint = Color.White,
+                )
             }
-            HeaderCriarConta()
-            Spacer(modifier = Modifier.height(72.dp))
-            CamposCriarConta()
-            Spacer(modifier = Modifier.height(98.dp))
-            createButtonWithWidth(
-                    textButton = stringResource(R.string.continuar) ,
-                    naveController = navController ,
-                    navName = "",
-                    corBotao = GreenKalos,
-                    width = 350.dp
-            )
-            Spacer(modifier = Modifier.height(41.dp))
-            Column (
-                modifier = Modifier.padding(30.dp)
-            ){
-                ContinueCom()
-            }
-
-
-
         }
 
+        HeaderCriarConta()
+
+        Spacer(modifier = Modifier.height(72.dp))
+        CamposCriarConta()
+        Spacer(modifier = Modifier.height(98.dp))
+        createButtonWithWidth(
+            textButton = stringResource(R.string.continuar),
+            naveController = navController,
+            navName = "",
+            corBotao = GreenKalos,
+            width = 350.dp
+        )
+        Spacer(modifier = Modifier.height(41.dp))
+        Column(
+            modifier = Modifier.padding(30.dp)
+        ) {
+            ContinueCom()
+        }
+        Row (
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = stringResource(R.string.ja_usuario),
+                color = Color.White,
+                fontSize = 12.sp
+            )
+
+            Text(
+                text = stringResource(R.string.faca_login),
+                fontSize = 12.sp,
+                color = GreenKalos,
+                modifier = Modifier.clickable { navController.navigate("fazerLogin") }
+            )
+        }
+        Spacer(modifier = Modifier.height(40.dp))
+
+    }
 }
 
 //@Preview(showSystemUi = true, showBackground = true)
