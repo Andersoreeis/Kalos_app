594c121dfc3f4c626e427e6ce8a0e8150fa50239
CLAUDIO HENRIQUE FERREIRA DE SOUSA <22282470@dsn127m1246728.cfp127aluno.local>
2023-09-11 16:51:25 -0300

    comecando integracao

--- app/src/main/java/br/senai/sp/jandira/kalos_app/model/BaseResponse.kt
+++ app/src/main/java/br/senai/sp/jandira/kalos_app/model/BaseResponse.kt
@@ -3,6 +3,7 @@ package br.senai.sp.jandira.kalos_app.model
 import com.google.gson.annotations.SerializedName
 
 data class BaseResponse<T>(
-    @SerializedName("aluno")
-    var aluno: T? = null
+    @SerializedName("alunos")
+    var data: T? = null
+
 )
