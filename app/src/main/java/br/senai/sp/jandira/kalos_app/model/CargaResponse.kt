aa651953ada996a2bac7146a82daa2418d177e59
YASMIN GONÇALVES COELHO <22282215@dsn127m1242267.cfp127aluno.local>
2023-10-25 16:54:21 -0300

    tentando fazer editar, arrumar o mutable state of qnd editar

--- app/src/main/java/br/senai/sp/jandira/kalos_app/model/CargaResponse.kt
+++ app/src/main/java/br/senai/sp/jandira/kalos_app/model/CargaResponse.kt
@@ -2,7 +2,7 @@ package br.senai.sp.jandira.kalos_app.model
 
 data class CargaResponse(
     val id: Int? = 0,
-    val peso: String? = "",
+    var peso: String? = "",
     val data_horario: String? = "",
     val id_aluno: Int? = 0,
     val id_exercicio_serie_repeticao: Int? = 0
--- app/src/main/java/br/senai/sp/jandira/kalos_app/service/RetrofitHelper.kt
+++ app/src/main/java/br/senai/sp/jandira/kalos_app/service/RetrofitHelper.kt
@@ -6,6 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitHelper {
     private const val BASE_URL = "https://kaloscorp.cyclic.cloud/"
     private const val BASE_URL2 = "http://10.107.144.11:8080/"
+    private const val BASE_URL3 = "http://10.107.144.2:8080/"
 
     fun getInstance(): Retrofit{
         return Retrofit.Builder()
