6fe5244d66c313d7a3d6c4a213a5ec07ae5f4832
YASMIN GONÇALVES COELHO <22282215@dsn127m1242267.cfp127aluno.local>
2023-12-01 16:37:57 -0300

    ajustes no banco

--- app/src/main/java/br/senai/sp/jandira/kalos_app/service/RetrofitHelper.kt
+++ app/src/main/java/br/senai/sp/jandira/kalos_app/service/RetrofitHelper.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitHelper {
     private const val BASE_URL = "https://kaloscorp.cyclic.app/"
     private const val BASE_URL2 = "http://10.107.144.3:8080/" //artur
-    private const val BASE_URL3 = "http://10.107.144.4:8080/"  //yasmin
+    private const val BASE_URL3 = "http://10.107.144.31:8080/"  //yasmin
 
     fun getInstance(): Retrofit{
         return Retrofit.Builder()
